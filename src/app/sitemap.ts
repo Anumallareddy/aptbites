03e83ea1cb04bab944b3d54dd98b13021d8f4206
Mallareddy Dasari <malladasari89@gmail.com>
2026-03-22 14:35:41 -0500

    Add robots and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://www.aptbites.com'
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/products`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/cart`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.4,
+    },
+  ]
+}
